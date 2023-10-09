fe6059a63bd3c1ae735933e56575ccba92b92e47
Rishi Ganesh <mr.rishiganesh@gmail.com>
2023-10-09 18:08:49 +0530

    Added a class to render the game
    Made the generateboard method functional.

--- main.js
+++ main.js
@@ -18,19 +18,45 @@ class MinesweeperGame {
         this.rows = rows;
         this.cols = cols;
         this.numMines = numMines;
-        this.board = []; 
-        this.generateBoard();
+        this.board = this.generateBoard();
     }
 
     generateBoard() {
+        const board = [];
         for (let row = 0; row < this.rows; row++) {
             let rowCells = [];
             for (let col = 0; col < this.cols; col++) {
                 const cell = new Cell(row, col);
                 rowCells.push(cell);
             }
-            this.board.push(rowCells);
+            board.push(rowCells);
+        }
+        return board;
+    }
+}
+
+class GameRenderer {
+    constructor(game, containerId) {
+        this.game = game;
+        this.container = document.getElementById(containerId);
+    }
+
+    render() {
+        for (let row = 0; row < this.game.rows; row++) {
+            const rowElement = document.createElement('div');
+            rowElement.className = 'row';
+
+            for (let col = 0; col < this.game.cols; col++) {
+                const cellElement = document.createElement('div');
+                cellElement.className = 'cell';
+                rowElement.appendChild(cellElement);
+            }
+
+            this.container.appendChild(rowElement);
         }
     }
 }
 
+const game = new MinesweeperGame(10, 10, 10);
+const gameRenderer = new GameRenderer(game, 'game-board');
+gameRenderer.render();
