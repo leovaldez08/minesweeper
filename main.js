cadd849d28fef943da74b29deac2d447e18a82e4
Rishi Ganesh <mr.rishiganesh@gmail.com>
2023-11-16 16:40:26 +0530

    Added an Observer Design Pattern

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+class Observer {
+    // Update method to be implemented by concrete observers
+    update() {}
+}
+
+class Subject {
+    // List to store observers
+    observers = [];
+
+    // Add an observer to the list
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    // Notifies all observers
+    notifyObservers() {
+        this.observers.forEach(observer => observer.update());
+    }
+}
+
 class Cell {
+    game = null;
+
+    // Cell properties
     constructor(row, col) {
         this.row = row;
         this.col = col;
@@ -6,31 +29,36 @@ class Cell {
         this.isRevealed = false;
         this.isFlagged = false;
         this.adjacentMines = 0;
-        this.game = null;
     }
 
+    // Set the MinesweeperGame instance for the cell
     setGame(game) {
         this.game = game;
     }
 
+    // Check if the cell contains a mine
     isMineCell() {
         return this.isMine;
     }
 
+    // Reveal the cell
     reveal() {
         if (this.isRevealed || this.isFlagged) {
             return;
         }
         this.isRevealed = true;
 
+        // Check for game win condition
         if (this.game) {
             const revealedCells = this.game.board.flat().filter(cell => cell.isRevealed);
             if (revealedCells.length === this.game.rows * this.game.cols - this.game.numMines) {
                 this.game.isGameOver = true;
+                this.game.notifyObservers(); 
             }
         }
     }
 
+    // To toggle flag on the cell
     toggleFlag() {
         if (!this.isRevealed) {
             this.isFlagged = !this.isFlagged;
@@ -38,6 +66,7 @@ class Cell {
     }
 }
 
+// Numbered cells for representing the number of adjacent mines
 class NumberedCell extends Cell {
     constructor(row, col, number) {
         super(row, col);
@@ -45,13 +74,20 @@ class NumberedCell extends Cell {
     }
 }
 
-class MinesweeperGame {
+class MinesweeperGame extends Subject {
+    rows;
+    cols;
+    numMines;
+    board;
+    isGameOver = false;
+
+    // MinesweeperGame properties
     constructor(rows, cols, numMines) {
+        super();
         this.rows = rows;
         this.cols = cols;
         this.numMines = numMines;
         this.board = this.generateBoard();
-        this.isGameOver = false;
     }
 
     setCells() {
@@ -62,6 +98,7 @@ class MinesweeperGame {
         }
     }
 
+    // Generating the initial game board
     generateBoard() {
         const board = [];
         for (let row = 0; row < this.rows; row++) {
@@ -77,22 +114,24 @@ class MinesweeperGame {
         return board;
     }
 
+    // Placing mines randomly on the board
     placeMines(board, numMines) {
-        let minesPlaced = 0;
-
-        while (minesPlaced < numMines) {
-            const randomRow = Math.floor(Math.random() * this.rows);
-            const randomCol = Math.floor(Math.random() * this.cols);
-
-            const cell = board[randomRow][randomCol];
-
-            if (!cell.isMine) {
-                cell.isMine = true;
-                minesPlaced++;
-            }
+        const totalCells = this.rows * this.cols;
+        const minePositions = new Set();
+    
+        while (minePositions.size < numMines) {
+            const randomPosition = Math.floor(Math.random() * totalCells);
+            minePositions.add(randomPosition);
         }
+    
+        minePositions.forEach(position => {
+            const row = Math.floor(position / this.cols);
+            const col = position % this.cols;
+            board[row][col].isMine = true;
+        });
     }
-
+    
+    // Calculating the number of adjacent mines for each cell
     calculateAdjacentMines(board) {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
@@ -113,6 +152,7 @@ class MinesweeperGame {
         }
     }
 
+    // Reveals all mines on the board
     revealAllMines() {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
@@ -123,8 +163,10 @@ class MinesweeperGame {
             }
         }
         this.isGameOver = true;
+        this.notifyObservers(); 
     }
 
+    // Recursive function to reveal adjacent cells
     revealAdjacentCells(row, col) {
         for (let i = row - 1; i <= row + 1; i++) {
             for (let j = col - 1; j <= col + 1; j++) {
@@ -141,6 +183,7 @@ class MinesweeperGame {
         }
     }
 
+    // Handling the reveal of a cell
     handleCellReveal(row, col) {
         const cell = this.board[row][col];
 
@@ -156,22 +199,31 @@ class MinesweeperGame {
         }
     }
 
+    // Checks for a winning condition
     checkForWin() {
         const revealedCells = this.board.flat().filter(cell => cell.isRevealed);
         if (revealedCells.length === this.rows * this.cols - this.numMines) {
             this.isGameOver = true;
+            this.notifyObservers(); 
         }
     }
 }
 
-class GameRenderer {
+class GameRenderer extends Observer {
+    game;
+    container;
+
+    // GameRenderer properties
     constructor(game, containerId) {
+        super();
         this.game = game;
         this.container = document.getElementById(containerId);
+        this.game.addObserver(this);
     }
 
+    // Renders the game UI
     render() {
-        this.container.innerHTML = ''; 
+        this.container.innerHTML = '';
 
         if (this.game.isGameOver) {
             this.displayGameOver();
@@ -180,6 +232,7 @@ class GameRenderer {
         }
     }
 
+    // Display the game board UI
     displayGameBoard() {
         for (let row = 0; row < this.game.rows; row++) {
             const rowElement = document.createElement('div');
@@ -194,14 +247,14 @@ class GameRenderer {
                     e.preventDefault();
                     if (e.button === 2) {
                         this.game.board[row][col].toggleFlag();
-                        this.render();
+                        this.game.notifyObservers(); 
                     }
                 });
 
                 // Handle left-click to reveal cells
                 cellElement.addEventListener('click', () => {
                     this.game.handleCellReveal(row, col);
-                    this.render();
+                    this.game.notifyObservers(); 
                 });
                 this.updateCellUI(cellElement, this.game.board[row][col]);
 
@@ -212,6 +265,7 @@ class GameRenderer {
         }
     }
 
+    // Display the game over screen
     displayGameOver() {
         const gameOverText = document.createElement('div');
         gameOverText.textContent = 'Game Over';
@@ -233,26 +287,30 @@ class GameRenderer {
             this.container.appendChild(rowElement);
         }
 
+        // Button to restart the game
         const restartButton = document.createElement('button');
         restartButton.textContent = 'Restart';
-        
+
+        // Creates a new game instance when the button is clicked
         restartButton.addEventListener('click', () => {
             const newGame = new MinesweeperGame(this.game.rows, this.game.cols, this.game.numMines);
             newGame.setCells();
             newGame.placeMines(newGame.board, newGame.numMines);
             newGame.calculateAdjacentMines(newGame.board);
             this.game = newGame;
+            this.game.addObserver(this); 
             this.render();
         });
 
         this.container.appendChild(restartButton);
     }
 
+    // Updates the UI of a cell based on its state
     updateCellUI(cellElement, cell) {
         if (cell.isRevealed) {
             if (cell.isMineCell()) {
                 cellElement.classList.add('mine');
-                cellElement.textContent = 'M'; 
+                cellElement.textContent = 'M';
             } else {
                 cellElement.textContent = cell.adjacentMines;
             }
@@ -263,12 +321,17 @@ class GameRenderer {
             }
         }
     }
+
+    // Calling this method when the observer is notified of a change
+    update() {
+        this.render();
+    }
 }
 
-// Initialization
-const game = new MinesweeperGame(10, 10, 5);
+// Initializing a game
+const game = new MinesweeperGame(10, 10, 6);
+const gameRenderer = new GameRenderer(game, 'game-board');
 game.setCells();
 game.placeMines(game.board, game.numMines);
 game.calculateAdjacentMines(game.board);
-const gameRenderer = new GameRenderer(game, 'game-board');
 gameRenderer.render();
